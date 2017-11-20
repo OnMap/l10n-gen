581ca033f1dc6c783bbcbd36032767a820405f28
PhilipKram <kram.phil@gmail.com>
2017-11-20 12:19:09 +0200

    WIP

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+
+const program = require('commander');
+
+const pckg = require('./package.json');
+const localizationFiles = require('./lib/parser/source-ios');
+
+
+program
+.version(pckg.version)
+
+program
+.arguments('<dir>')
+.description('')
+.action(function(dir){
+    // let dir2 = process.argv[3]
+    // console.log(dir2)
+    let found = localizationFiles(dir)
+    console.log(found)
+})
+
+program.parse(process.argv);
