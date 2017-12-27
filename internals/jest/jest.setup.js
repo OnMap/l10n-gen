c94041fddb69e38260f79883298bd227111ae9b9
PhilipKram <kram.phil@gmail.com>
2017-12-27 11:33:38 +0200

    wip

--- internals/jest/jest.setup.js
+++
@@ -1,5 +0,0 @@
-// Example of a test regexp for a group of test suites
-// "testRegex": "(tests/api/middleware/.+\\.js$|tests/core/mongo/.+\\.js$)",
-
-// To enable running this file before each test
-// "setupFiles": ["./internals/jest/jest.setup.js"],
